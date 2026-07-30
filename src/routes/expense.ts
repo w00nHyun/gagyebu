671bc3f7e41603f215f5e9950fb668dd02ce6330
w00nHyun <unhyeon412@gmail.com>
2026-07-30 17:09:17 +0900

    내역 수정 및 삭제 api 추가

--- src/routes/expense.ts
+++ src/routes/expense.ts
@@ -1,13 +1,13 @@
 import express, { Router, Request, Response, NextFunction } from 'express';
-import {ObjectId, db } from '../config/database';
+import { ObjectId, db } from '../config/database';
 import requireAuth from '../middleware/auth.middleware';
 import validateExpense from '../middleware/validation.middleware';
 const router: Router = express.Router();
 
 
 interface expenseInfo extends UserPayLoad {
-  userId : string,
-  username : string,
+  userId: string,
+  username: string,
   event: string,
   category: string,
   price: number,
@@ -17,17 +17,17 @@ interface expenseInfo extends UserPayLoad {
   date: string,
 }
 
-interface UserPayLoad{
-  username :string,
-  userId:string
+interface UserPayLoad {
+  username: string,
+  userId: string
 }
 
 
 
 
-router.get('/write',requireAuth ,(req: Request, res: Response) => {
+router.get('/write', requireAuth, (req: Request, res: Response) => {
   try {
-    
+
     res.render('expenseWrite.ejs');
   } catch (error) {
     console.log(error)
@@ -40,18 +40,18 @@ router.get('/write',requireAuth ,(req: Request, res: Response) => {
 //가계부 지출 내역 작성 api
 router.post('/post', validateExpense, async (req: Request, res: Response) => {
   try {
-    
+
 
     //document에 담아야할 내용들을 req.body에서 받기
     const { event, category, price, explanation, isFixed, date } = req.body;
-    
-    const {userId,username} = req.user as UserPayLoad;
+
+    const { userId, username } = req.user as UserPayLoad;
     console.log(req.user);
     //user 정보 받아서 넣기... 
     //req.user로 받기
     console.log(req.user);
     //고정 수입인지 확인
-    const fix : boolean = (isFixed === 'on');
+    const fix: boolean = (isFixed === 'on');
     //기본 인터페이스에게서 타입 받아오기
     const baseInfo: expenseInfo = {
       event,
@@ -65,7 +65,7 @@ router.post('/post', validateExpense, async (req: Request, res: Response) => {
       username
     };
     // fix의 bool타입에 의해서 타입 정하기
-    const result: expenseInfo  = baseInfo;
+    const result: expenseInfo = baseInfo;
     await db.collection('transection').insertOne(result);
     res.status(200).json({
       success: true,
@@ -80,14 +80,15 @@ router.post('/post', validateExpense, async (req: Request, res: Response) => {
 
 
 //지출 내역 api
-router.get('/list',requireAuth ,async (req: Request, res: Response) => {
+router.get('/list', requireAuth, async (req: Request, res: Response) => {
   try {
-    let user  = req.user as UserPayLoad;
+    let user = req.user as UserPayLoad;
     let result: expenseInfo[] = await db.collection<expenseInfo>('transection').find({
-      userId : user.userId
-      ,moneyType: 'expense' }).toArray();
+      userId: user.userId
+      , moneyType: 'expense'
+    }).toArray();
     const total: number = result.reduce((sum, item) => sum + item.price, 0);
-    
+
     res.render('expenseList.ejs', { items: result, total: total });
   } catch (error) {
     console.log(error)
@@ -99,14 +100,69 @@ router.get('/list',requireAuth ,async (req: Request, res: Response) => {
 
 
 //고정 지출 내역 api
-router.get('/fixedCost/list',requireAuth, async (req: Request, res: Response) => {
+router.get('/fixedCost/list', requireAuth, async (req: Request, res: Response) => {
   try {
-    let user  = req.user as UserPayLoad;
-    let result: expenseInfo[] = await db.collection<expenseInfo>('transection').find({ userId : user.userId,isFixed: true }).toArray();
+    let user = req.user as UserPayLoad;
+    let result: expenseInfo[] = await db.collection<expenseInfo>('transection').find({ userId: user.userId, isFixed: true }).toArray();
     const total: number = result.reduce((sum, item) => sum + item.price, 0);
     res.render('expenseFixed.ejs', { items: result, total: total });
   } catch (error) {
     console.log(error)
+  }
+})
+
+//수정 화면 접속
+router.get('/edit/:id', async (req: Request, res: Response) => {
+  let documentId = req.params.id as string;
+
+  let result = await db.collection<expenseInfo>('transection').findOne({
+    _id: new ObjectId(documentId)
+  })
+  if (!result) {
+    return res.send(404);
+  }
+  res.render('expenseEdit.ejs', { result: result });
+})
+
+//수정 put 요청
+router.put('/edit/:id', async (req: Request, res: Response) => {
+  //put 요청을 받아서
+  //수정된 값을 수정하기
+  let id: string = req.params.id as string;
+  const { event, category, price, explanation, isFixed, date } = req.body;
+  await db.collection<expenseInfo>('transection').updateOne({
+    _id: new ObjectId(id)
+  }, {
+    $set: {
+      category: category,
+      price: price,
+      event: event,
+      explanation: explanation,
+      isFixed: isFixed,
+      date: date
+    }
+  })
+  res.status(200).json({
+    success: true,
+    message: "저장 완료"
+  });
+
+})
+
+//삭제 delete 요청 
+router.delete('/delete/:id', async (req: Request, res: Response) => {
+  try {
+    let id: string = req.params.id as string;
+    let result = await db.collection<expenseInfo>('transection').deleteOne({ _id: new ObjectId(id) })
+    if (result.deletedCount === 1) {
+      res.json({ success: true, message: '지출 내역이 삭제되었습니다.' });
+    } else {
+      // id에 해당하는 데이터가 DB에 존재하지 않았을 경우
+      res.status(404).json({ success: false, message: '삭제할 지출 내역을 찾을 수 없습니다.' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: '삭제 처리 중 오류가 발생했습니다.' });
   }
 })
 
